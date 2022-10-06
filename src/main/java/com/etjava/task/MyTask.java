4af8e73c4cd8d1bf09d5ddbe9c881020a663fd78
etjava <etjava@hotmail.com>
2022-10-06 15:10:14 +0800

    Spring 自带定时任务demo

---
+++ src/main/java/com/etjava/task/MyTask.java
@@ -0,0 +1,12 @@
+package com.etjava.task;
+
+public class MyTask {
+
+	public void show() { 
+	    System.out.println("show method 1"); 
+	  } 
+	 
+	  public void print() { 
+	    System.out.println("print method 1"); 
+	  } 
+}
